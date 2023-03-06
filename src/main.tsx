8764e647126bcb10fb2047a60b83e72c8f5e1f80
Dani <24252795+SGD14@users.noreply.github.com>
2023-03-06 12:17:07 +0100

    Add podcast details route

--- src/main.tsx
+++ src/main.tsx
@@ -15,6 +15,10 @@ const router = createBrowserRouter([
         path: "",
         element: <PodcastList />,
       },
+      {
+        path: "podcast/:podcastId",
+        element: <div>Podcast Page</div>
+      }
     ],
   },
 ]);
--- src/pages/podcast-list/PodcastCard.tsx
+++ src/pages/podcast-list/PodcastCard.tsx
@@ -1,4 +1,5 @@
 import { Podcast } from "@/types";
+import { Link } from "react-router-dom";
 
 interface PodcastCardProps {
   podcast: Podcast;
@@ -6,12 +7,15 @@ interface PodcastCardProps {
 
 export default function PodcastCard({ podcast }: PodcastCardProps) {
   return (
-    <div className="bg-gray-50 shadow-md flex flex-col items-center gap-2 p-4 mt-20">
+    <Link
+      to={`podcast/${podcast.id}`}
+      className="bg-gray-50 shadow-md flex flex-col items-center gap-2 p-4 mt-20 hover:bg-gray-200"
+    >
       <img src={podcast.imgUrl} className="rounded-full -mt-20" />
       <h1 className="font-bold text-lg">{podcast.name}</h1>
       <p className="font-bold text-sm text-gray-600">
         Author: {podcast.author}
       </p>
-    </div>
+    </Link>
   );
 }
