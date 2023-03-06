390190cf691496123d8c52b5549066e3ba7fadb9
Dani <24252795+SGD14@users.noreply.github.com>
2023-03-06 10:26:18 +0100

    Create useTop100Podcasts hook

---
+++ src/hooks/useTop100Podcasts.ts
@@ -0,0 +1,26 @@
+import { Podcast } from "@/types";
+import { useQuery } from "@tanstack/react-query";
+
+const fetchPodcasts = async (): Promise<Podcast[]> => {
+  const response = await fetch(
+    "https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json"
+  );
+
+  if (response.ok) {
+    const json = await response.json();
+
+    return json.feed.entry.map((podcastJson: any) => ({
+      name: podcastJson["im:name"].label,
+      author: podcastJson["im:artist"].label,
+      imgUrl: podcastJson["im:image"][2].label,
+    }));
+  }
+
+  throw Error("Error while loading podcasts");
+};
+
+export default function useTop100Podcasts() {
+  const podcastsQuery = useQuery(["podcasts"], fetchPodcasts);
+
+  return podcastsQuery;
+}
