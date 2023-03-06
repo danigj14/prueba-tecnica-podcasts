c0ca90925d511b102831d351220ded8cea25a452
Dani <24252795+SGD14@users.noreply.github.com>
2023-03-06 09:11:17 +0100

    Create base route + layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,21 @@
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link, Outlet } from "react-router-dom";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="w-screen h-screen bg-gray-100">
+      <header className="container mx-auto p-3 flex justify-between items-center border-b border-gray-300">
+        <nav>
+          <Link to="/" className="text-lg text-blue-600">
+            Podcaster
+          </Link>
+        </nav>
+        <FontAwesomeIcon icon={faSpinner} spin size="lg" />
+      </header>
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  );
 }
